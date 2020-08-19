d6dd4a1bdc81d94063b611ba617e3c11f7d4054c
19026852 <19026852@LAPTOP-0A08J5MD>
2020-08-19 12:38:11 +0800

    update PurchaseOrder and MenuItem class

---
+++ C206_CaseStudy/src/MenuItem.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19026852, Aug 19, 2020 12:34:10 PM
+ */
+
+public class MenuItem {
+
+}
