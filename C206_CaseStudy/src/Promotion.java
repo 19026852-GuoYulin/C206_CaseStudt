fc0aa0374fe4ec09f6399978f83c6994140a690c
19009772 <19009772@172.24.85.187>
2020-08-19 13:15:54 +0800

    Update Promotion

---
+++ C206_CaseStudy/src/Promotion.java
@@ -0,0 +1,45 @@
+
+
+public class Promotion {
+	private String promotionCode;
+	private String endDate;
+	private double discountAmount;
+	
+	public Promotion(String promotionCode, String endDate, double discountAmount) {
+		this.promotionCode = promotionCode;
+		this.endDate = endDate;
+		this.discountAmount = discountAmount;
+	}
+
+	public String getPromotionCode() {
+		return promotionCode;
+	}
+
+	public void setPromotionCode(String promotionCode) {
+		this.promotionCode = promotionCode;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public double getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public void setDiscountAmount(double discountAmount) {
+		this.discountAmount = discountAmount;
+	};
+	
+	public String toString() {
+		String promoCode = super.toString();
+		return String.format("%-63s %-20d", promoCode, getPromotionCode());
+		
+	}
+
+
+}
