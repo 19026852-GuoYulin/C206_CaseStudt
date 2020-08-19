623d6bdfbd973ed4f120b80615d5bb7db1b70223
19026852 <19026852@LAPTOP-0A08J5MD>
2020-08-19 12:41:27 +0800

    update menu item

--- C206_CaseStudy/src/MenuItem.java
+++ C206_CaseStudy/src/MenuItem.java
@@ -7,6 +7,45 @@
  * 19026852, Aug 19, 2020 12:34:10 PM
  */
 
-public class MenuItem {
+public class MenuItem extends CanteenApp{
+	
+	private String category ;
+	private String name ;
+	private double price ;
+
+	public MenuItem(String category, String name, double price) {
+		this.category = category;
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String toString() {
+		String MIinfo = super.toString();
+		return String.format("%-63s", MIinfo);
+	}
 
 }
